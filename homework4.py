4c482d420a9b9659d5697e03c035b9aa6ab3faee
KirillGromov13 <buzzlik@icloud.com>
2024-07-18 13:51:53 +0300

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -1,7 +1,5 @@
-my_string = input('Введите заклинание: ')
+my_string = input('Введите Ваши ФИО: ')
 print(len(my_string))
-
-my_string = 'Мальчик, который выжил, пришел умереть'
 print(my_string.upper())
 print(my_string.lower())
 print(my_string.replace(' ', ''))
