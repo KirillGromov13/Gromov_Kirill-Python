b480f9061b577b6a0ffd6951134e3d05ee200e5d
KirillGromov13 <buzzlik@icloud.com>
2024-07-18 08:59:33 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+number_of_homework = 12
+number_of_hours = 1.5
+name_of_course = 'Python'
+one_lesson = number_of_hours / number_of_homework
+print('Курс:', name_of_course, ',', 'Всего задач:', number_of_homework, ',', 'Затрачено часов:',number_of_hours, ',', 'Среднее время выполнения:', one_lesson)
+
